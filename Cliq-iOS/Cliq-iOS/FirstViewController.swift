1a78fd03254ba937f10f97357f8329ff772e07fe
Arjun Sarode <sarodearjun57@gmail.com>
2014-10-11 14:49:59 -0500

    Initial Commit

---
+++ Cliq-iOS/Cliq-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cliq-iOS
+//
+//  Created by Arjun Sarode on 10/11/14.
+//  Copyright (c) 2014 Cliq. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

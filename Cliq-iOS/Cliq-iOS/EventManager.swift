a3a48133119aa921649293f3dfa4d2b1b767191e
Arjun Sarode <sarodearjun57@gmail.com>
2014-10-14 01:43:26 -0500

    Create addEvent function

--- Cliq-iOS/Cliq-iOS/EventManager.swift
+++ Cliq-iOS/Cliq-iOS/EventManager.swift
@@ -13,7 +13,7 @@ var eventMgr : EventManager = EventManager()
 struct event{
     var desc = "No description"
     var tags = "No tags"
-    var timePosted = 1413234046
+    var timePosted = NSDate.date()
     var groupSize = 6
     var location = ["lat": 40.110971, "long": -88.225928]
     var name = "Firstname"
@@ -27,6 +27,17 @@ class EventManager: NSObject {
     
     //TODO
     // Add a function to add a new event with the information in the parameters
+    
+    func addEvent(desc: String, tags: String, timePosted: NSDate,
+        groupSize: Int, lat: Double, long: Double, name: String, avatar: String) {
+        
+            var location = ["lat": lat, "long": long]
+            
+            events.append(event(desc: desc, tags: tags, timePosted: timePosted,
+                groupSize: groupSize, location: location, name: name, avatar: avatar))
+    
+    }
+
     
 //    func addEvent(title: String, desc: String) {
 //        events.append(event(title: title, desc: desc))
